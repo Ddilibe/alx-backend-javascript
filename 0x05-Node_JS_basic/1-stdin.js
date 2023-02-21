1025fb8ebaad50f5674de5e96b3bc482bd12d057
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-21 06:44:33 +0100

    [Chore]: Done with task 0 and 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,21 @@
-console.log("Welcome to Holberton School, what is your name?");
-const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout,
+console.log = (d) => {
+	process.stdout.write(`${d}\n`);
+}
+const well = "Welcome to Holberton School, what is your name?";
+console.log(well);
+process.on('exit', () =>{
+	process.stdout.write("This important software is now closing");
 });
-if (process.argv.length > 1) {
-	console.log(`Your name is ${process.argv[2]} from processes`);
+
+if (process.stdin.isTTy) {
+	process.stdin.on('readable', () => {
+		const input = process.stdin.read();
+		process.stdout.write(`Your name is: ${input.toString()}`);
+		process.exit()
+	});
 } else {
-	readline.question(``, name=>{
-		console.log(`Your name is ${name} from readline`);
-		readline.close();
+	process.stdin.on('data', (data) => {
+		process.stdout.write(`Your name is ${data}`);
+		process.exit();
 	});
 }
-console.log("\nThis important software is now closing\n")
