51cad26d439f7546bb5597d206850cb1d0602812
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-22 01:15:13 +0100

    [Chore]: Done with some complex analysis

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,16 @@
-console.log = (d) => {
-	process.stdout.write(`${d}\n`);
-}
-const well = "Welcome to Holberton School, what is your name?";
-console.log(well);
-process.on('exit', () =>{
-	process.stdout.write("This important software is now closing");
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTy) {
-	process.stdin.on('readable', () => {
-		const input = process.stdin.read();
-		process.stdout.write(`Your name is: ${input.toString()}`);
-		process.exit()
-	});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
 } else {
-	process.stdin.on('data', (data) => {
-		process.stdout.write(`Your name is ${data}`);
-		process.exit();
-	});
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
 }
--- 0x05-Node_JS_basic/README.md
+++ 0x05-Node_JS_basic/README.md
@@ -1,1 +1,1 @@
-# Node JS Basics
+# Node Js Basic
